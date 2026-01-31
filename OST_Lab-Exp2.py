eaf729e25b7dfc0d1f0f0b17d67b8274e3d09a8e
Shivangini Gupta <shivangini2006@gmail.com>
2026-01-31 10:40:43 +0530

    Updated Python file with function

--- OST_Lab-Exp2.py
+++ OST_Lab-Exp2.py
@@ -1,3 +1,13 @@
 # OST_Lab-Exp2.py
-print("Hello Git!")
-print("Experiment 2: Version Control with Git (Local Repository)")
+# print("Hello Git!")
+# print("Experiment 2: Version Control with Git (Local Repository)")
+
+# OST_Lab-Exp2.py
+
+# Version 2 - after changes
+
+def greet():
+    print("Hello Git!")
+    print("Learning Git version control using Python")
+
+greet()
