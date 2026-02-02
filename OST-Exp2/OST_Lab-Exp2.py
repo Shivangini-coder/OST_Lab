fd5a8d9295602be5ea31be35b46deea0284aec12
Shivangini Gupta <shivangini2006@gmail.com>
2026-02-02 22:32:11 +0530

    Added Experiment 2 files properly

---
+++ OST-Exp2/OST_Lab-Exp2.py
@@ -0,0 +1,13 @@
+# Version -1 (before modification)
+
+# print("Hello Git!")
+# print("Experiment 2: Version Control with Git (Local Repository)")
+
+
+# Version -2 (after modification)
+
+def greet():
+    print("Hello Git!")
+    print("Learning Git version control using Python")
+
+greet()
